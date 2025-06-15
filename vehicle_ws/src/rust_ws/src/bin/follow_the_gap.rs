4e26010d19b9997ffaedb831dda9fdcfe6fe9360
Jisang <journeyofbabo@gmail.com>
2025-06-15 23:25:24 +0900

    filted lidar ranges

--- vehicle_ws/src/rust_ws/src/bin/follow_the_gap.rs
+++ vehicle_ws/src/rust_ws/src/bin/follow_the_gap.rs
@@ -1,4 +1,5 @@
 use ackermann_msgs::msg::AckermannDriveStamped;
+use anyhow::{Error, Result};
 use rclrs::*;
 use sensor_msgs::msg::LaserScan;
 use std::f32::consts::PI;
@@ -18,7 +19,7 @@ impl ReactiveFollowGap {
 
         let scan_subscription =
             node.create_subscription::<LaserScan, _>("/scan", move |msg: LaserScan| {
-                if let Err(e) = Self::process_and_publish_filtered(msg, &publisher_clone) {
+                if let Err(e) = Self::lidar_callback(msg) {
                     eprintln!("Error during scan process: {}", e);
                 }
             })?;
@@ -29,10 +30,49 @@ impl ReactiveFollowGap {
         })
     }
 
-    fn process_and_publish_filtered(
-        msg: LaserScan,
-        publisher: &Publisher<LaserScan>,
-    ) -> Result<(), Error> {
+    fn process_lidar(msg: &LaserScan) -> Vec<LaserScan> {
+        // Preprocess the LiDAR scan array. Expert implementation includes:
+        // 1.Setting each value to the mean over some window
+        // 2.Rejecting high values (eg. > 3m)
+
+        const MAX_LIDAR_RANGE: f32 = 3.0;
+
+        let filtered_points: Vec<f32> = msg.ranges
+            .iter()
+            .map(|&range| if range <= MAX_LIDAR_RANGE { range } else { MAX_LIDAR_RANGE})
+            .collect();
+
+        let mut processed_scan = msg.clone();
+        processed_scan.ranges = filtered_points;
+
+        vec![processed_scan]
+
+    }
+
+    fn find_max_gap() {
+        // Return the start index & end index of the max gap in free_space_ranges
+    }
+
+    fn find_best_point() {
+        // Start_i & end_i are start and end indicies of max-gap range, respectively
+        // Return index of best point in ranges
+        // Naive: Choose the furthest point within ranges and go there
+    }
+
+    fn lidar_callback(msg) -> Result<(), Error> {
+        // Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
+        let lidar_points = Self::process_lidar(&msg);
+
+        // Todo
+        // Find closest point to LiDAR
+
+        // Eliminate all points inside 'bubble' (set them to zero)
+
+        // Find max length gap
+
+        // Find the best point in the gap
+
+        // Publish Drive message
         Ok(())
     }
 }

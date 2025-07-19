5f8297a4b633eb6b88c78a22e6d7df0e9ed8928e
Jisang <journeyofbabo@gmail.com>
2025-07-19 20:18:05 +0900

    update launch

--- rust_f1tenth_ws/src/launch_pkg/launch/reactive_launch.py
+++ rust_f1tenth_ws/src/launch_pkg/launch/reactive_launch.py
@@ -5,18 +5,27 @@ from launch_ros.actions import Node
 def generate_launch_description():
     env_vars = os.environ.copy()
     env_vars['CONFIG1_PATH'] = './src/rust_ws/config.toml'
-    env_vars['CONFIG_PATH'] = './src/toml_param/vehicle_param.toml'
+    # env_vars['CONFIG_PATH'] = './src/toml_param/vehicle_param.toml'
+    env_vars['CONFIG_PATH'] = './src/follow_the_gap_param/vehicle_param.toml'
     # config_path = './src/rust_ws/config.toml'
     
     return LaunchDescription([
         Node(
-            package='rust_ws',
-            executable='toml_config',
-            name='rust_ws_config_node',
+            package='follow_the_gap_param',
+            # executable='follow_the_gap_param', 
+            executable='hybrid_reactive', 
+            name='reactive_follow',
             output='screen',
             env=env_vars
-            # env={'CONFIG_PATH': config_path}
         ),
+        # Node(
+        #     package='rust_ws',
+        #     executable='toml_config',
+        #     name='rust_ws_config_node',
+        #     output='screen',
+        #     env=env_vars
+        #     # env={'CONFIG_PATH': config_path}
+        # ),
         # Node(
         #     package='toml_param',
         #     executable='toml_reactive', 
@@ -24,11 +33,11 @@ def generate_launch_description():
         #     output='screen',
         #     env=env_vars
         # ),
-        Node(
-            package='toml_param',
-            executable='hybrid_reactive', 
-            name='hybrid_reactive',
-            output='screen',
-            env=env_vars
-        ),
+        # Node(
+        #     package='toml_param',
+        #     executable='hybrid_reactive', 
+        #     name='hybrid_reactive',
+        #     output='screen',
+        #     env=env_vars
+        # ),
     ])
